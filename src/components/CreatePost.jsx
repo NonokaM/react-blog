e3c2195653490e7241135230f39d104204cca5e2
nonokaM <c1201071@gmail.com>
2023-06-13 23:55:44 +0900

    記事投稿画面追加

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./CreatePost.css"
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <div className="createPostPage">
+      <div className="postContainer">
+        <h1>記事を投稿する</h1>
+        <div className="inputPost">
+          <div>タイトル</div>
+          <input type="text" pleceholder="タイトルを入力" />
+        </div>
+        <div className="inputPost">
+          <div>投稿</div>
+          <textarea pleceholder="投稿内容を記入" />
+        </div>
+        <button>投稿する</button>
+      </div>
+    </div>
   )
 }
 

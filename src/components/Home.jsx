5fa08fcbb99078f382755a9cc43e7eb95796452d
nonokaM <c1201071@gmail.com>
2023-06-18 22:35:43 +0900

    feat: homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./Home.css"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>title</h1>
+        </div>
+        <div className="postTextContainer">
+          ブログの記事内容＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@NonokaM</h3>
+          <button>delete</button>
+        </div>
+      </div>
+    </div>
   )
 }
 

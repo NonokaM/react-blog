78d42dae1b419ac5faa4432007c4b3cd0d48a89d
nonokaM <c1201071@gmail.com>
2023-06-19 04:18:18 +0900

    feat: 記事の取得・表示

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { collection, getDocs } from "firebase/firestore"
+import { db } from "../firebase"
 import "./Home.css"
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      // console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    }
+    getPosts();
+  }, []);
+
   return (
     <div className="homePage">
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>title</h1>
+      {postList.map((post) => {
+        return post.author ? (
+          <div className="postContents" key={post.id}>
+          <div className="postHeader">
+            <h1>{post.title}</h1>
+          </div>
+          <div className="postTextContainer">
+            {post.postsText}
+          </div>
+          <div className="nameAndDeleteButton">
+            <h3>@{post.author.username}</h3>
+            <button>delete</button>
+          </div>
         </div>
-        <div className="postTextContainer">
-          ブログの記事内容＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
-        </div>
-        <div className="nameAndDeleteButton">
-          <h3>@NonokaM</h3>
-          <button>delete</button>
-        </div>
-      </div>
+        ) : null;
+      })}
     </div>
   )
 }
